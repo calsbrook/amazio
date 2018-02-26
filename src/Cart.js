38717c8e2da4d9551117f5141fcfc34245ed4e2d
Cameron Alsbrook <CameronAlsbrook@gmail.com>
2018-02-26 10:16:32 -0800

    initial commit

---
+++ src/Cart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Button, Icon} from 'react-materialize'
+
+export default () => (
+	<div>
+		<Button waves='light'>
+			<Icon>shopping_cart</Icon>
+			<span  class="badge">2</span>
+		</Button>
+	</div>
+)
